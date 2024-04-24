1f1f23f424d8cb62f5ce497725feb35c31ed70d9
Nikhil Rachawar <nikhil123rachawar@gmail.com>
2024-04-24 22:44:33 +0530

    Main header component and styles added to layout

---
+++ components/main-header.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import logoPng from "@/assets/logo.png";
+
+import classes from "./main-header.module.css";
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <img src={logoPng.src} alt="Page Logo" />
+        Next Level Food
+      </Link>
+
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href="/meals">Browse Meals</Link>
+          </li>
+          <li>
+            <Link href="/community">Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
